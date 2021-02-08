6cf39439dbc9beaec8f183a9e3e220e151408a5f
olter1991 <teeereeen@yandex.ru>
2021-02-09 01:35:28 +0300

    homework_7.2_Airport done

---
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/Main.java
@@ -0,0 +1,26 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar d1 = new GregorianCalendar();
+        Date now = d1.getTime();
+
+        Calendar d2 = new GregorianCalendar();
+        d2.add(Calendar.HOUR, -2);
+        Date lastTwoHours = d2.getTime();
+
+        Airport pulkovo = Airport.getInstance();
+
+        pulkovo.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(f -> (f.getDate().after(lastTwoHours) & f.getDate().before(now) & f.getType() == (Flight.Type.ARRIVAL)))
+                .forEach(f -> System.out.println(f.getAircraft().getModel() + "  /  " +  f.getDate()));
+        System.out.println("\n" + now);
+    }
+    }
+
