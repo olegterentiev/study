14642208331786016000f0b7777df0aec931fb21
olter1991 <teeereeen@yandex.ru>
2021-08-12 23:32:27 +0300

    homework_13.1 done

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,22 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long result = 0;
+
+        try {
+            File folder = new File(path);
+            for (File f : folder.listFiles())
+                if (f.isDirectory()) {
+                    String path2 = f.getAbsolutePath();
+                    result += calculateFolderSize(path2);
+                } else {
+                    result += f.length();
+                }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+        import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        for (; ; ) {
+            System.out.println("Введите путь к папке");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            if  (!name.matches("^[A-Za-z]:\\/[\\w/]+")){
+                System.out.println("Неверный путь");
+                continue;
+            }
+            File file = new File(name);
+            double gigabyte = FileUtils.calculateFolderSize(name) / Math.pow(1024, 3);
+            String read = String.format("Размер папки '%s' составляет : %.2f Гб", file.getAbsolutePath(), gigabyte );
+            System.out.println(read);
+        }
     }
 }
