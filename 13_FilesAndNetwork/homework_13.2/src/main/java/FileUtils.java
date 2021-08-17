1a292378ccc27f9048a58a96cb09c4b4052539b0
olter1991 <teeereeen@yandex.ru>
2021-08-18 00:17:54 +0300

    homework_13.2(copy) done

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,16 +1,63 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+    public static void copyFolderWalkFileTree(String sourceDirectory, String destinationDirectory) throws IOException {
+        Path source = Paths.get(sourceDirectory);
+        Path destination = Paths.get(destinationDirectory);
+        Files.walkFileTree(source, new SimpleFileVisitor<>() {
+            public FileVisitResult visitFile(Path src, BasicFileAttributes fileAttributes) {
+                Path des = destination.resolve(source.relativize(src));
+                try {
+                    Files.copy(src, des, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("Скопирован файл из    " + src + "    в    " + des);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return FileVisitResult.CONTINUE;
+            }
+            public FileVisitResult preVisitDirectory(Path src, BasicFileAttributes fileAttributes) {
+                Path des = destination.resolve(source.relativize(src));
+                try {
+                    Files.copy(src, des, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("Скопирована папка из    " + src + "    в    " + des);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
         File source = new File(sourceDirectory);
         File destination = new File(destinationDirectory);
-        try {
-            Files.copy(source,destination);
-        } catch (Exception ex){
-            ex.printStackTrace();
+        if (source.isDirectory()) {
+            File[] s = source.listFiles();
+            if(s.length == 0){
+                System.out.println("Скопирована пустая папка    " + sourceDirectory + "    в    " + destinationDirectory);
+            }
+            if (!destination.exists()) {
+                destination.mkdir();
+            }
+            String files[] = source.list();
+            for (String file : files) {
+                File srcFile = new File(source, file);
+                File destFile = new File(destination, file);
+                copyFolder(srcFile.getAbsolutePath(), destFile.getAbsolutePath());
+            }
+        } else {
+            InputStream in = new FileInputStream(source);
+            OutputStream out = new FileOutputStream(destinationDirectory);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.close();
+            System.out.println("Файл скопирован из    " + sourceDirectory + "    в    " + destinationDirectory);
         }
-
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введите путь к папке которую нужно скопировать с помощью Files.walkFileTree()");
+        Scanner scannerIn = new Scanner(System.in);
+        String pathIn = scannerIn.nextLine();
+
+        System.out.println("Введите путь к папке в которую нужно скопировать");
+        Scanner scannerOut = new Scanner(System.in);
+        String pathOut = scannerOut.nextLine();
+
+        try {
+            FileUtils.copyFolderWalkFileTree(pathIn, pathOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("------------------------------------------------------------");
+
+        System.out.println("Введите путь к папке которую нужно скопировать с помощью рекурсии");
+        Scanner scannerIn2 = new Scanner(System.in);
+        String pathIn2 = scannerIn2.nextLine();
+
+        System.out.println("Введите путь к папке в которую нужно скопировать");
+        Scanner scannerOut2 = new Scanner(System.in);
+        String pathOut2 = scannerOut2.nextLine();
+
+        FileUtils.copyFolder(pathIn2, pathOut2);
     }
 }
