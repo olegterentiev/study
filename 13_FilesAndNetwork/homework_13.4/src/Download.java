cfa82598db58e663ff255eeb98fdddee5f15e296
olter1991 <teeereeen@yandex.ru>
2021-08-29 18:06:35 +0300

    homework_13.4 done

--- 13_FilesAndNetwork/homework_13.4/src/Download.java
+++ 13_FilesAndNetwork/homework_13.4/src/Download.java
@@ -1,2 +1,45 @@
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Download {
+
+    public static void downloadFile(String path) throws IOException {
+        String[] fragments = path.split("/");
+        String nameFile = fragments[fragments.length - 1];
+        if (nameFile.endsWith(".jpg")) {
+            System.out.println(nameFile);
+            URL url = new URL(path);
+            File file = new File("images", nameFile);
+
+            if (!file.exists()) {
+                file.createNewFile();
+                InputStream in = new BufferedInputStream(url.openStream());
+                OutputStream out = new FileOutputStream("images/" + fragments[fragments.length - 1]);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+                in.close();
+                out.close();
+            }
+        }
+    }
+
+    public static String parserFile(String file) {
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(file));
+            lines.forEach(line -> builder.append(line).append("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+
+    }
 }
--- 13_FilesAndNetwork/homework_13.4/src/Main.java
+++ 13_FilesAndNetwork/homework_13.4/src/Main.java
@@ -1,2 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String code = Download.parserFile("data/code.html");
+        Document doc = Jsoup.parse(code);
+        Elements elements = doc.select("img");
+        for (Element el : elements) {
+            String path = el.absUrl("src");
+            Download.downloadFile(path);
+        }
+    }
 }
