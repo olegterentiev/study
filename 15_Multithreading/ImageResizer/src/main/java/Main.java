e417d991023f4805c713a0f026c63afa18ac4e61
olter1991 <teeereeen@yandex.ru>
2022-01-24 16:02:09 +0300

    homework_15.1 use ThreadPool and remainder of division

--- 15_Multithreading/ImageResizer/src/main/java/Main.java
+++ 15_Multithreading/ImageResizer/src/main/java/Main.java
@@ -2,11 +2,15 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
+
     private  static int newWidth = 300;
     private static int cores = Runtime.getRuntime().availableProcessors();
+    public static final int MAX_T = cores;
     public static void main(String[] args) {
 
         String srcFolder = "C://image_resizer/src";
@@ -15,6 +19,7 @@ public class Main {
         File srcDir = new File(srcFolder);
         long start = System.currentTimeMillis();
         File[] files = srcDir.listFiles();
+        ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
 
         for (int i = 0 ; i < cores; i++ ){
             assert files != null;
@@ -23,7 +28,17 @@ public class Main {
             File[] file = new File[files.length / cores];
             System.arraycopy(files, size,file,0,file.length);
             ImageResizer resizer = new ImageResizer(newWidth,dstFolder,file,start);
-            resizer.start();
+            pool.execute(resizer);
+            if(i == cores - 1){                                                                      // получение последней итерации цикла
+                if(!(files.length%cores == 0)){                                                      // проверка на остаток
+                    int startPoint = files.length - files.length%cores;                              // переменная старта копирования
+                    File[] lastFile = new File[files.length%cores];                                  // создание массива остатка
+                    System.arraycopy(files,startPoint,lastFile,0,lastFile.length);           // процесс копирования
+                    ImageResizer lastResizer = new ImageResizer(newWidth,dstFolder,lastFile,start);  // создание экземпляра
+                    pool.execute(lastResizer);                                                       // занесение в пул потоков
+                }
+            }
         }
+        pool.shutdown();
     }
 }
--- 15_Multithreading/Transactions/src/Bank.java
+++ 15_Multithreading/Transactions/src/Bank.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Bank {
